0d6d3b4b8824d3928916d4031cd6d0b1c8251c34
Kush Bhardwaj <122326048+KUSH-Q-37@users.noreply.github.com>
2026-09-11 22:49:28 +0530

    Update SITE_URL in sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const SITE_URL = "https://portfolio-wine-delta-35.vercel.app";
+const SITE_URL = "https://kush-bhardwaj.vercel.app/";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
